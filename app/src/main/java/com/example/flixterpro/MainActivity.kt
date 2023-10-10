1c69f87393ace5718332f9f9dbac0763bf0a9474
Norsang <thenorsang@gmail.com>
2023-10-10 15:31:57 -0400

    updated

--- app/src/main/java/com/example/flixterpro/MainActivity.kt
+++ app/src/main/java/com/example/flixterpro/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.flixterpro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val reviewButton = findViewById<Button>(R.id.Review)
+        reviewButton.setOnClickListener {
+            val intent = Intent(this, testpage.xml)
+            startActivity(intent)
+        }
     }
 }
